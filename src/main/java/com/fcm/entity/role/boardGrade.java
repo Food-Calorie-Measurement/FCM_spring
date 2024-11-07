8a237aba9ea85bf8ed0cd4f76b4c0154f7600ce5
this <lee855916234@gmail.com>
2024-11-07 19:44:50 +0900

    boardGrade enum 타입 변수 upperCase 설정

--- src/main/java/com/fcm/entity/role/boardGrade.java
+++ src/main/java/com/fcm/entity/role/boardGrade.java
@@ -1,5 +1,12 @@
 package com.fcm.entity.role;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum boardGrade {
-    QUESTION, COMMUNITY
+    QUESTION, COMMUNITY;
+
+    @JsonCreator
+    public static boardGrade fromString(String key) {
+        return key == null ? null : boardGrade.valueOf(key.toUpperCase());
+    }
 }
